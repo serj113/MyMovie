55a00ef518c75070fb243a06158d89aea1150ff9
setia <putra.simaremare@gmail.com>
2019-07-27 23:47:13 +0700

    added room and refine api_key

---
+++ app/src/main/java/com/example/mymovie/room/MovieDao.kt
@@ -0,0 +1,21 @@
+package com.example.mymovie.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.mymovie.model.Movie
+import io.reactivex.Single
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM contact")
+    fun getMovies(): Single<List<Movie>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(movie: Movie)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(users: List<Movie>)
+}
--- app/src/main/java/com/example/mymovie/util/Constants.kt
+++ app/src/main/java/com/example/mymovie/util/Constants.kt
@@ -1,4 +1,5 @@
 package com.example.mymovie.util
 
 /** The base URL of the API */
-const val BASE_URL: String = "http://178.128.94.81:3000/"
+const val BASE_URL: String = "https://api.themoviedb.org/3/"
+const val API_KEY: String = "0db16e52aeb64bb11168a6b456e8d140"
